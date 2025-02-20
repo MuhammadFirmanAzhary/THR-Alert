bf64c490d74d7c6135e3c79425983779932d7013
muhammadfirmanazhary <muhammadfirmanazhary.com>
2025-02-20 22:19:15 +0700

    first-commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+function playSound() {
+    document.getElementById("thrAudio").play();
+  }
+  
+  function openModal() {
+    document.getElementById("modal").classList.add("active");
+  }
+  
+  function closeModal() {
+    document.getElementById("modal").classList.remove("active");
+  }
